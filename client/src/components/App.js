f6a49fda6387176955f75aa94609537d88b0b30b
Julian Hoyos <jhoyos135@gmail.com>
2019-03-03 16:52:37 -0500

    fixed pagination request from the landing page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -48,29 +48,38 @@ class App extends Component {
 
   up = () => {
     let next = this.state.page.number
+    let search = document.querySelector('#search').value;
+    if(search === '') {
+      this.setState({
+        search: {
+          query: 'trending'
+        },
+        page: {
+          number: next + 1
+        }
+      })
+    };
     this.setState({
-
       page: {
         number: next + 1
       }
     })
+
   };
   down = () => {
     let prev = this.state.page.number
     this.setState({
-
       page: {
         number: prev - 1
       }
     })
-  }
+  };
   prev = () => {
     if(this.state.page.number !== 1) {
       this.down();
     }
     if(this.state.page.number === 1) {
       document.querySelector('.prev').classList.add('disabled');
-      
     }
   }
   next = () => {
